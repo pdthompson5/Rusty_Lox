3c170f50049d33d2f4b638009b5ea6ed1a2425b6
pdthompson5 <pdthompson5@crimson.ua.edu>
2022-04-19 09:34:19 -0500

    Add lib.rs so integration tests work

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -1,3 +1,12 @@
 
 
-// #[test]
+use std::fs;
+fn read_file(filename: &str) -> String{
+    fs::read_to_string(filename).expect("Failed to find test file")
+}
+
+#[cfg(test)]
+fn test_test(){
+    print!("{}", read_file("tests/resources/all_tests.lox"));
+    assert!(false)
+}
